e987f86be1f99dff6555ccefd339d5fdc0555c1d
Farhood Etaati <farhoodetaati@gmail.com>
2024-12-06 01:14:27 +0330

    Adds prometheus metrics to the server

--- service/server.go
+++ service/server.go
@@ -12,11 +12,41 @@ import (
 	"time"
 
 	_ "github.com/lib/pq"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/sethvargo/go-envconfig"
 	"github.com/sirupsen/logrus"
 )
 
+// Declare Prometheus metrics
+var (
+	totalRequests = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of requests handled by the server",
+		},
+		[]string{"path"},
+	)
+	requestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Histogram of response durations",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"path"},
+	)
+	requestErrors = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_request_errors_total",
+			Help: "Total number of error requests received",
+		},
+		[]string{"path", "error"},
+	)
+)
+
+// ApiConfig is the api config, and is configurable via environment variables.
 type ApiConfig struct {
+	// DB Config
 	DBHost         string `env:"DB_HOST, default=localhost"`
 	DBPort         int    `env:"DB_PORT, default=5432"`
 	DBUser         string `env:"DB_USER, default=postgres"`
@@ -27,6 +57,8 @@ type ApiConfig struct {
 	DBMaxIdleConns int    `env:"DB_MAX_IDLE_CONNS, default=512"`
 	DBMaxLifeTime  int    `env:"DB_MAX_LIFETIME_SECS, default=20"`
 	DBMaxIdleTime  int    `env:"DB_MAX_IDLETIME_SECS, default=10"`
+	// Server Port
+	ServerPort int `env:"SERVER_PORT, default=3333"`
 }
 type IpApiResponseBody struct {
 	Query   string
@@ -55,21 +87,25 @@ type ApiHandler struct {
 	config     *ApiConfig
 }
 
+// writeSuccessResponse writes the data and the ok status code for a successful request.
 func writeSuccessResponse(w *http.ResponseWriter, body *ApiSuccessResponseData) {
 	message, _ := json.Marshal(body)
 	(*w).Write(message)
 }
 
+// writeApiError writes the message and the error status code for an erroneus request.
 func writeApiError(w *http.ResponseWriter, errorMessage string) {
 	message, _ := json.Marshal(ApiErrorResponseData{errorMessage})
 	(*w).WriteHeader(http.StatusInternalServerError)
 	(*w).Write(message)
 }
 
+// validateIp validates whether an string is an ip address or not
 func validateIp(ip *string) bool {
 	return net.ParseIP(*ip) != nil
 }
 
+// getIpCountryFromCache reads the cache to check whether the requested information exists and if so, it will return that.
 func (h *ApiHandler) getIPCountryFromCache(ip string) (*string, error) {
 	query := fmt.Sprintf("SELECT id, ip, country FROM %s WHERE ip =$1;", h.config.DBTableName)
 	h.logger.Infof("row query: %s", query)
@@ -89,6 +125,7 @@ func (h *ApiHandler) getIPCountryFromCache(ip string) (*string, error) {
 	}
 }
 
+// writeIpCountryToCache writes the data fetched externally to the cache table in the db.
 func (h *ApiHandler) writeIpCountryToCache(ip string, country string) error {
 	query := fmt.Sprintf("INSERT INTO %s (ip, country) VALUES ('%s', '%s');", h.config.DBTableName, ip, country)
 	h.logger.Infof("Running insert query: %s", query)
@@ -99,6 +136,7 @@ func (h *ApiHandler) writeIpCountryToCache(ip string, country string) error {
 	return nil
 }
 
+// getIPCountryFromWeb gets the requested information from a public api and returns it to the app.
 func (h *ApiHandler) getIPCountryFromWeb(ip string) (*string, error) {
 	base_url := "http://ip-api.com"
 	// sample requeset: http://ip-api.com/json/24.48.0.1
@@ -131,12 +169,17 @@ func (h *ApiHandler) getIPCountryFromWeb(ip string) (*string, error) {
 	return &data.Country, nil
 }
 
+// ipLocationHandler is the handler func that handles the incoming requests for the main endpoint.
 func (h *ApiHandler) ipLocationHandler(w http.ResponseWriter, r *http.Request) {
+	timer := prometheus.NewTimer(requestDuration.WithLabelValues(r.URL.Path))
+	defer timer.ObserveDuration()
+	totalRequests.WithLabelValues(r.URL.Path).Inc()
 	h.logger.Infof("Received request: Method=%s, URL=%s, Headers=%v", r.Method, r.URL.String(), r.Header)
 	// X-Real-IP is the IP of the client
 	ip := r.Header.Get("X-Real-IP")
 	if !validateIp(&ip) {
 		h.logger.Errorf("Bad IP address given, returning error.")
+		requestErrors.WithLabelValues(r.URL.Path, "bad_ip").Inc()
 		writeApiError(&w, "bad ip address")
 		return
 	}
@@ -154,6 +197,7 @@ func (h *ApiHandler) ipLocationHandler(w http.ResponseWriter, r *http.Request) {
 	// if cannot get it from web, terminate the request and return error
 	if err != nil {
 		h.logger.Errorf("Cannot get the ip from web, got this error: %s", err)
+		requestErrors.WithLabelValues(r.URL.Path, "web_fetch_error").Inc()
 		writeApiError(&w, "internal error")
 		return
 	}
@@ -162,6 +206,7 @@ func (h *ApiHandler) ipLocationHandler(w http.ResponseWriter, r *http.Request) {
 	err = h.writeIpCountryToCache(ip, *country)
 	if err != nil {
 		h.logger.Errorf("Cannot write the data to db, got this error: %s", err)
+		requestErrors.WithLabelValues(r.URL.Path, "db_write_error").Inc()
 	}
 	writeSuccessResponse(&w, &ApiSuccessResponseData{*country})
 }
@@ -191,8 +236,15 @@ func (config *ApiConfig) CreateDBConnection() (*sql.DB, error) {
 
 func main() {
 	// initialization
+	//// Init logging
 	logger := logrus.New()
 
+	//// Init metrics
+	prometheus.MustRegister(totalRequests)
+	prometheus.MustRegister(requestDuration)
+	prometheus.MustRegister(requestErrors)
+
+	//// Init database connection
 	ctx := context.Background()
 	var config ApiConfig
 	if err := envconfig.Process(ctx, &config); err != nil {
@@ -205,13 +257,16 @@ func main() {
 	}
 	defer db.Close()
 
+	//// Init http client
 	httpClient := http.Client{Timeout: 10 * time.Second}
 	defer httpClient.CloseIdleConnections()
 
+	// Create the http webserver and listen on the desired port
 	handler := ApiHandler{db, logger, &httpClient, &config}
 	http.HandleFunc("/", handler.ipLocationHandler)
-
-	http_err := http.ListenAndServe(":3333", nil)
+	// Expose the Prometheus metrics endpoint
+	http.Handle("/metrics", promhttp.Handler())
+	http_err := http.ListenAndServe(fmt.Sprintf(":%d", config.ServerPort), nil)
 	if http_err != nil {
 		panic(http_err)
 	}
